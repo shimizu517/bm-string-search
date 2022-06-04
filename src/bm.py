39557a8ba9752a196f9dd36dbbe0d8eadd1369dc
yuta <namtok.dev@gmail.com>
2022-06-04 13:44:53 +0900

    feature: bm string search

--- src/bm.py
+++ src/bm.py
@@ -1,9 +1,17 @@
-from typing import Dict
+from collections import defaultdict
+from typing import DefaultDict
 
 
-def make_km_table(pattern: str) -> Dict[str, int]:
-    table = dict()
-    raise Exception("TODO")
+def make_km_table(pattern: str) -> DefaultDict[str, int]:
+    """
+    Returns a defaultdict for bad rules.
+    The key is an alphabet in the pattern and the value is the size to slide.
+    The default value is len(pattern)
+    """
+    m = len(pattern)
+    table = defaultdict(lambda: m)
+    for i, c in enumerate(pattern):
+        table[c] = m - i - 1
     return table
 
 
@@ -13,11 +21,29 @@ class Bm(object):
         self.pattern = pattern
         self.table = make_km_table(pattern)
 
-    def decide_slide_width(self, c: str) -> int:
-        assert len(c) == 1
-        raise Exception("TODO")
-        return -1
-
     def search(self) -> int:
-        raise Exception("TODO")
+        """
+        Search the first pattern occurrence in text.
+        Use Bad Character Heuristic to decide slide size.
+        Returns: the index of the first character of matched substring of self.text.
+            e.g., When self.text is ANPANMAN and self.pattern is ANM, it returns 3 (ANP[ANM]AN).
+        """
+        n = len(self.text)
+        m = len(self.pattern)
+        k = m - 1  # k is the index of the character of the current substring of text being compared with pattern
+        while k < n:
+            j = m - 1  # l is the index of the character of the pattern that is being compared.
+            while 0 <= j:
+                if self.text[k] == self.pattern[j]:
+                    k -= 1
+                    j -= 1
+                else:
+                    break
+            else:
+                # match
+                return k + 1
+            # Align self.text[k] with self.pattern[m].
+            # It goes forward by one when it goes backward by k += self.table[self.text[k]].
+            k += max(self.table[self.text[k]], m - j)
+
         return -1
